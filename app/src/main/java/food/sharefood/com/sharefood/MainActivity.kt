09ab0c787493f803861cdb8d093ab629c14a857e
unzilakhan <unzila15@gmail.com>
2018-10-19 11:19:25 +0500

    initial commit

---
+++ app/src/main/java/food/sharefood/com/sharefood/MainActivity.kt
@@ -0,0 +1,12 @@
+package food.sharefood.com.sharefood
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/food/sharefood/com/sharefood/SplashActivity.kt
@@ -0,0 +1,11 @@
+package food.sharefood.com.sharefood
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
